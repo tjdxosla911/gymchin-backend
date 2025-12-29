8fd74738414a305c2bd2f53c2ad8941e6befa33d
freefree87st <tjdxosla911@gmail.com>
2025-12-29 22:38:16 +0900

    init: spring boot skeleton about gymchin

---
+++ src/main/java/com/gymchin/api/GymchinApiApplication.java
@@ -0,0 +1,13 @@
+package com.gymchin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymchinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymchinApiApplication.class, args);
+	}
+
+}
